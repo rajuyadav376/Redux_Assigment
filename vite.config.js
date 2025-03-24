65dd2197facea24ebed401e2709b343fb4819a57
rajuyadav376 <sumantidevi9737@gmail.com>
2025-03-24 23:41:41 +0530

    Bulid the deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",  // 👈 Fixes incorrect asset paths on Netlify
   plugins: [react()],
 })
